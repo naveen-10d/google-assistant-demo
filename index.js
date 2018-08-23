76785dd9fbfc559b123e5749c7e0c4d3e2323599
Navyn <navyn10d@gmail.com>
2018-08-23 17:17:49 +0530

    params added
    params added

--- index.js
+++ index.js
@@ -14,16 +14,32 @@ app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
     console.log("-------webhook activated---------")
     var speech = req.body.result.metadata.intentName;
-    if(speech === 'Address and Location'){
+    if (speech === 'Address and Location') {
         console.log("<<<<<<<<<<<< First Intent >>>>>>>>>>>>")
         var nameSlot = req.body.result.parameters.name;
-      console.log("nameSlot>>>>",nameSlot)
+        console.log("nameSlot>>>>", nameSlot)
+    } else {
+        return res.json({
+            speech: 'please, tell me the name',
+            displayText: 'please, tell me the name',
+            source: "webhook-google-assistant-sample"
+        });
     }
+
+
+    var data =
+        req.body.result &&
+            req.body.result.parameters &&
+            req.body.result.parameters.facilities
+            ? req.body.result.parameters.facilities
+            : "Seems like some problem. Speak again.";
+    console.log("speech>>>>>>>>>>>>>>>>>>.", data)
     return res.json({
         speech: 'please, tell me the name',
         displayText: 'please, tell me the name',
         source: "webhook-google-assistant-sample"
     });
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
